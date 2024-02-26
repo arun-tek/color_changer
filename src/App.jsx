27d784845a7d436a2d384b33b49710efcc7b61ad
Arun Tekwani <aruntekwani4567@gmail.com>
2024-02-26 08:39:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  const [bgcolor, setBgColor] = useState("Red")
+
+  return (
+    <div style={{backgroundColor: bgcolor}} className='h-screen w-screen'>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() =>{setBgColor("red")}}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+          <button
+          onClick={() =>{setBgColor("green")}}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+          <button
+          onClick={() =>{setBgColor("blue")}}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
